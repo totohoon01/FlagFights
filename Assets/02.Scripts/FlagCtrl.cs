1e4fa94d391967c75a6f3a4a769671c38a31c892
Hoon <totohoon01@naver.com>
2021-04-29 17:44:36 +0900

    flag design, get Flage Done

---
+++ Assets/02.Scripts/FlagCtrl.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FlagCtrl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.CompareTag("Player"))
+        {
+            transform.SetParent(coll.transform);
+
+            foreach (var mesh in GetComponentsInChildren<MeshRenderer>())
+            {
+                mesh.enabled = false;
+            }
+            GetComponentInChildren<TMP_Text>().enabled = true;
+            //해당 오브젝트의 차일드로 들어간다.
+            //깃발을 숨기고
+            //해당 플레이어의 머리 위에 깃발 소유여부 표시!
+        }
+    }
+}
